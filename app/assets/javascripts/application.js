3b0de16f0a9b051ac502ca58ede3a6675e518bee
Ruben <ruben.hartog+github@gmail.com>
2013-04-07 14:28:00 +0200

    Better upload button, tinymce tweaks.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap.min
-//= require jasny-bootstrap.min
+//= require complete-bootstrap.min
 //= require tinymce-jquery
 //= require_tree .
